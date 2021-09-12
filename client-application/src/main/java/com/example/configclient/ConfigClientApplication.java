b0edcfb327130f6c0455a4fab8d1d767d0bb5060
Harshal Pande <kkool.harshal@gmail.com>
2021-09-12 22:21:42 +0530

    Working Solution
    Signed-off-by: Harshal Pande <kkool.harshal@gmail.com>

--- client-application/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ client-application/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -1,13 +1,26 @@
 package com.example.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/")
 public class ConfigClientApplication {
+	
+	@Value("${message}")
+	private String message;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
+	
+	@GetMapping(value = "hello")
+	public String helloWorld() {
+		return "From Config Server " + message; 
+	}
+	
 
 }
